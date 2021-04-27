256803fc8013964f6db0e08b55151ebaf0681d09
Zidni <zidni@Zidnis-MacBook-Air.local>
2021-04-27 20:43:37 +0700

    Create Data Serialize

---
+++ app/src/main/java/com/interview/app/model/Data.kt
@@ -0,0 +1,13 @@
+package com.interview.app.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Food(
+    @SerializedName(value = "name")
+    val foodName: String?,
+    @SerializedName(value = "image")
+    val image: String?,
+    @SerializedName(value = "desc")
+    val desc: String?,
+
+)
